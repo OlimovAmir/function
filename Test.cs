329a228534b47e4cb8954febfae5f9ce462448cd
olimov Amir <olimov.amir@mail.ru>
2023-11-07 18:07:55 +0300

    Первый калькулятор

---
+++ Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function
+{
+     class Test
+    {
+        static void Main(string[] args)
+        {
+            
+            int a, b, c;
+
+            Console.WriteLine("Вводите данный a: ");
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Вводите данный b: ");
+
+            b = int.Parse(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("total : " + c);
+        }
+    }
+}
